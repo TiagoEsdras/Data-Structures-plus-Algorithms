fa33e40a66b8ccc53e29290fbf79e562edfb3abe
Tiago Esdras <tiagoesdrasvieira@gmail.com>
2022-12-03 10:01:46 -0300

    Creating ContainsDuplicate Algorithms - LeetCode

---
+++ ContainsDuplicate/ContainsDuplicate/Program.cs
@@ -0,0 +1,16 @@
+﻿//Given an integer array nums, return true if any value appears at least twice in the array,
+//and return false if every element is distinct.
+
+/**
+ * @param {int[]} nums
+ * @return {bool}
+ */
+
+static bool ContainsDuplicate(int[] nums)
+{
+    var numsWithOutRepeatNumbers = nums.Distinct().ToArray();
+    return numsWithOutRepeatNumbers.Length < nums.Length;
+}
+
+int[] nums = new int[3] { 1, 3, 4 };
+Console.WriteLine(ContainsDuplicate(nums));
