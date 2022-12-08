cc4e982817873bc4673c2a8dca05c7cab589d975
Tiago Esdras <tiagoesdrasvieira@gmail.com>
2022-12-08 17:27:06 -0300

    Creating FirtsRecurringNumber Algorithms

---
+++ FirstRecurringNumber/FirstRecurringNumber/Program.cs
@@ -0,0 +1,19 @@
+﻿//Given an array int[], return the first recurring number, or null if all number are unique
+
+using System.Collections;
+
+static int? FirtsRecurringNumber(int[] arr)
+{
+    var hash = new Hashtable();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (hash.ContainsKey(arr[i])) return arr[i];
+
+        hash.Add(arr[i], i);
+    }
+
+    return null;
+}
+
+var arr = new int[5] { 1, 7, 5, 4, 2 };
+Console.WriteLine(FirtsRecurringNumber(arr));
