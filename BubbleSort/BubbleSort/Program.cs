74d3de67af86697f0f82afb3a5c83b711447bf7d
Tiago Esdras <tiagoesdrasvieira@gmail.com>
2023-01-07 13:25:08 -0300

    Creating BubbleSort

---
+++ BubbleSort/BubbleSort/Program.cs
@@ -0,0 +1,20 @@
+﻿static int[] BubbleSort(int[] arr)
+{
+    int length = arr.Length;
+    for (int i = 0; i < length; i++)
+    {
+        for (int j = 0; j < length - 1 - i; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                (arr[j + 1], arr[j]) = (arr[j], arr[j + 1]);
+            }
+        }
+    }
+
+    return arr;
+}
+
+int[] arr = new int[10] { 1, 22, 33, 14, 5, 76, 77, 58, 39, 10 };
+var arrSorted = BubbleSort(arr);
+Console.WriteLine(String.Join(" ", arrSorted));
