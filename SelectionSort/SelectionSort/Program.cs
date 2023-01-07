67a5b6256f6cba3a801ae30acd44604d76f7dfbd
Tiago Esdras <tiagoesdrasvieira@gmail.com>
2023-01-07 15:08:51 -0300

    Creating Selection Sort

---
+++ SelectionSort/SelectionSort/Program.cs
@@ -0,0 +1,26 @@
+﻿static int[] SelectionSort(int[] arr)
+{
+    int length = arr.Length;
+    for (int i = 0; i < length; i++)
+    {
+        int smallNumberIndex = i;
+        for (int j = i + 1; j < length; j++)
+        {
+            if (arr[j] < arr[smallNumberIndex])
+            {
+                smallNumberIndex = j;
+            }
+
+            if (j == length - 1)
+            {
+                (arr[i], arr[smallNumberIndex]) = (arr[smallNumberIndex], arr[i]);
+            }
+        }
+    }
+
+    return arr;
+}
+
+int[] arr = new int[10] { 100, 22, 33, 14, 5, 76, 77, 58, 39, 1 };
+var arrSorted = SelectionSort(arr);
+Console.WriteLine(String.Join(" ", arrSorted));
