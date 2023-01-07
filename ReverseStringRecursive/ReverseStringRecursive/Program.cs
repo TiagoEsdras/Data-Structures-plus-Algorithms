896eb3dd0baf0d55c5901ee68d98e8438ecb3672
Tiago Esdras <tiagoesdrasvieira@gmail.com>
2023-01-06 22:36:13 -0300

    Creating Reverse String Iterative and Recursive

---
+++ ReverseStringRecursive/ReverseStringRecursive/Program.cs
@@ -0,0 +1,24 @@
+﻿static string ReverseStringRecursive(string str)
+{
+    if (str.Length == 0)
+        return str;
+
+    return ReverseStringRecursive(str[1..]) + str[0];
+}
+
+Console.WriteLine(ReverseStringRecursive("abcdefghij"));
+
+static string ReverseStringIterative(string str)
+{
+    var arrChar = str.ToCharArray();
+    var n = str.Length - 1;
+
+    for (int i = 0; i < n / 2; i++)
+    {
+        (arrChar[n - i], arrChar[i]) = (arrChar[i], arrChar[n - i]);
+    }
+
+    return new string(arrChar);
+}
+
+Console.WriteLine(ReverseStringIterative("abcdefghij"));
