ab3e6cc515c0bf4c705277f14aa82c297aded63a
Tiago Esdras <tiagoesdrasvieira@gmail.com>
2023-01-08 09:29:05 -0300

    Creating Insertion Sort

---
+++ InsertionSort/InsertionSort/Program.cs
@@ -0,0 +1,20 @@
+﻿static int[] InsertionSort(int[] arr)
+{
+    for (int i = 1; i < arr.Length; i++)
+    {
+        int j = i - 1;
+        int temp = arr[i];
+        while (j >= 0 && arr[j] > temp)
+        {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = temp;
+    }
+
+    return arr;
+}
+
+int[] arr = new int[10] { 100, 22, 33, 14, 5, 76, 77, 58, 39, 10 };
+var arrSorted = InsertionSort(arr);
+Console.WriteLine(String.Join(" ", arrSorted));
