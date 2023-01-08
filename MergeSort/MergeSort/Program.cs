9e033710d8ead5e751d9cbddaacf00225f2aaaf0
Tiago Esdras <tiagoesdrasvieira@gmail.com>
2023-01-08 10:47:22 -0300

    Creating Merge Sort

---
+++ MergeSort/MergeSort/Program.cs
@@ -0,0 +1,40 @@
+﻿static int[] MergeSort(int[] arr)
+{
+    int length = arr.Length;
+
+    if (length == 1) return arr;
+
+    int middle = length / 2;
+
+    int[] left = arr.Take(middle).ToArray();
+    int[] right = arr.Skip(middle).ToArray();
+
+    return Merge(MergeSort(left), MergeSort(right));
+}
+
+static int[] Merge(int[] left, int[] right)
+{
+    List<int> result = new();
+    int leftIndex = 0;
+    int rigthIndex = 0;
+
+    while (leftIndex < left.Length && rigthIndex < right.Length)
+    {
+        if (left[leftIndex] < right[rigthIndex])
+        {
+            result.Add(left[leftIndex]);
+            leftIndex++;
+        }
+        else
+        {
+            result.Add(right[rigthIndex]);
+            rigthIndex++;
+        }
+    }
+
+    return result.Concat(left.Skip(leftIndex)).Concat(right.Skip(rigthIndex)).ToArray();
+}
+
+int[] arr = new int[11] { 1, 22, 33, 14, 5, 76, 77, 58, 39, 10, 54 };
+var arrSorted = MergeSort(arr);
+Console.WriteLine(String.Join(" ", arrSorted));
